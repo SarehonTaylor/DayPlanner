82c7b030727acab601b3e4ebddb571ea09f21866
Sarehon Taylor <sarehontaylor@icloud.com>
2021-02-27 00:44:48 -0500

    timer is now enabled on the activity tracker

--- Develop/script2.js
+++ Develop/script2.js
@@ -1,24 +1,23 @@
+var timeBlocks = $(".container");
 
-//SETTING OF CURRENT PC'S TIME
-$("#currentDay").text(day);
+var timeDisplay = moment().format('MMMM Do YYYY hh:mm:ss');
 
-$("#currentDate").text(date);
-
-$("#currentTime").text(time);
-
+var timeDiv = $("#currentTime");
+timeDiv.append(timeDisplay);
 //GENERATE BLOCKS OF TIME
+//COLOR CODE DAY BASED ON PAST,PRESENT,FUTURE
+//WHEN PAGE REFRESH USER DATA REMAINS SAVED LOCALLY
 
 //WHEN PAGE REFRESH USER DATA REMAINS SAVED LOCALLY
 //SAVE USER INUPT DATA
 $(document).ready(function(){
 $('.savebutton').on('click', function(){
 let value = $(this).siblings('.description').val();
-let time = $(this).parent().attr('id')
-
 localStorage.setItem(time,value)
 })
 function hour(){
 let currenthour = moment().hours()
+
 //COLOR CODE DAY BASED ON PAST,PRESENT,FUTURE
 $('.time-block').each(function(){
 let blockhour = parseInt($(this).attr('id').split('-')[1])
@@ -27,15 +26,26 @@ if(blockhour < currenthour){
 }
 else if (blockhour === currenthour){
     $(this).addClass('present');
+    $(this).removeClass('past');
+    $(this).addClass('present');
     $(this).removeClass('past');    
 
 } else{
     $(this).removeClass('present');
     $(this).removeClass('past');
     $(this).addClass('future');
-    }
 
-});
+    $(this).removeClass('present');
+    $(this).removeClass('past');
+    $(this).addClass('future');
+}
+
+})
+
 
 }
-hour();
+
+
+
+
+});

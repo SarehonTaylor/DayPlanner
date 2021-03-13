c3562453e67626786ffe1258eb8b3778981fd2b0
Sarehon Taylor <sarehontaylor@icloud.com>
2021-03-13 15:29:12 -0500

    fixed issues with .js file not saving to local storage

--- script2.js
+++ script2.js
@@ -1,53 +1,37 @@
-$(document).ready(function () {
-  $(".saveBtn").on("click", function () {
-    let value = $(this).siblings(".description").val()
-    let time = $(this).parents().attr("id")
+$(document).ready(function() {
+  $(".saveBtn").on("click", function() {
+    let value = $(this).siblings(".description").val();
+    let time = $(this).parent().attr("id");
     localStorage.setItem(time, value);
   });
-  function hour() {
-    let currenthour = moment().hours();
-
-
-    //COLOR CODE DAY BASED ON PAST,PRESENT,FUTURE
-    $(".time-block").each(function () {
-      let blockhour = parseInt($(this).attr("id").split("-")[1]);
-      if (blockhour < currenthour) {
+  function hourUpdater() {
+    var currentHour = moment().hours();
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currentHour) {
         $(this).addClass("past");
-      } else if (blockhour === currenthour) {
+      } 
+      else if (blockHour === currentHour) {
         $(this).removeClass("past");
         $(this).addClass("present");
-      } else {
-        $(this).removeClass("present");
+      } 
+      else {
         $(this).removeClass("past");
+        $(this).removeClass("present");
         $(this).addClass("future");
       }
     });
   }
-    hour();
-  var interval = setInterval(hour, 15000);
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"))
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"))
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"))
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"))
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"))
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"))
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"))
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"))
-  $("#currentDay").text(moment().format("MMMM Do YYYY hh:mm:ss"))
-
-// localStorage.getItem(time, value)
-//   var timeDisplay = moment().format("MMMM Do YYYY hh:mm:ss");
-
-  // var timeDiv = $("#currentTime");
-  // timeDiv.append(timeDisplay);
-  //GENERATE BLOCKS OF TIME
-  //WHEN PAGE REFRESH USER DATA REMAINS SAVED LOCALLY
-
-  //WHEN PAGE REFRESH USER DATA REMAINS SAVED LOCALLY
-  //SAVE USER INUPT DATA
- 
- 
-  
-  
+  hourUpdater();
+  var interval = setInterval(hourUpdater, 15000);
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
